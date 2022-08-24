efa3b17ec0f88271c4fc1447b02483ef816286af
Andrei Danelli <danelliandrei@gmail.com>
2022-08-24 10:47:34 -0300

    :sparkles: added language pt-br and added most six people

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,7 @@
 insert into event (id, title, datini, datfim) values (1, 'teste', now(), '2022-10-10');
+insert into event (id, title, datini, datfim) values (2, 'teste2', now(), '2022-10-10');
+insert into event (id, title, datini, datfim) values (3, 'reunião - Samuel', now(), '2022-10-10');
+insert into event (id, title, datini, datfim) values (4, 'reunião - Pedro', now(), '2022-09-10');
+insert into event (id, title, datini, datfim) values (5, 'reunião - João', now(), '2022-10-25');
+insert into event (id, title, datini, datfim) values (6, 'reunião - Guilherme', now(), '2022-08-29');
+insert into event (id, title, datini, datfim) values (7, 'reunião - Pedrinho', now(), '2022-10-01');
